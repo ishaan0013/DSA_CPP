9b079ba1f8f0a47bae1450020a5c2924ec2ee5fc
Ishaan Bhardwaj <ishaanbhardwaj0013@gmail.com>
2023-12-17 18:07:17 +0530

    First Commit

---
+++ Check_prime.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n)
+{
+    if (n <= 1)
+    {
+        return false;
+    }
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    int ans, a;
+    cout << "Enter numebr:\n";
+    cin >> a;
+    ans = isPrime(a);
+    (ans == 0) ? cout << "Not Prime" : cout << "Prime";
+}
