ae8f9acc227532923c384359d7ac543c64f3d069
Ishaan Bhardwaj <ishaanbhardwaj0013@gmail.com>
2024-01-11 00:02:10 +0530

    Square root

---
+++ LeetCode/sqrt.cpp
@@ -0,0 +1,50 @@
+// 69. Sqrt(x)
+
+// Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+// You must not use any built-in exponent function or operator.
+// For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+
+//  Example 1:
+// Input: x = 4
+// Output: 2
+// Explanation: The square root of 4 is 2, so we return 2.
+
+// Example 2:
+// Input: x = 8
+// Output: 2
+// Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
+
+#include <iostream>
+#include <vector>
+using namespace std;
+void Square(long int num)
+{
+    int start = 0;
+    long int end = num;
+    long int mid = start + ((end - start) / 2);
+    int ans;
+    while (start <= end)
+    {
+        if (mid * mid == num)
+        {
+            ans = mid;
+            break;
+        }
+        else if (mid * mid > num)
+        {
+            end = mid - 1;
+        }
+        else if (mid * mid < num)
+        {
+            ans = mid;
+            start = mid + 1;
+        }
+        mid = start + ((end - start) / 2);
+    }
+    cout << ans;
+}
+int main()
+{
+    long int num = 78;
+    Square(num);
+}
